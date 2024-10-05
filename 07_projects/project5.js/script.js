a8b071b32d8cd6a9f61de0d140236583ddb2364b
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-10-05 15:05:50 +0530

    Chameleon - Automatic BG Changer

---
+++ 07_projects/project5.js/script.js
@@ -0,0 +1,29 @@
+const randomcolor = function(){
+    //hex code of color conatins 16 values 0-F
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+    //a color combination have 6 values 
+    for(let i=0;i<6;i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+ let intervalid;
+
+const changecolor = function(){
+    document.body.style.backgroundColor = randomcolor();
+}
+
+document.querySelector('#start').addEventListener('click',function(){
+    if(intervalid==null){
+        intervalid = setInterval(changecolor,2000)
+    }
+})
+
+
+document.querySelector('#stop').addEventListener('click',function(){
+    console.log('stopped')
+    clearInterval(intervalid)
+    intervalid=null
+})
--- 09_async.js/script.js
+++ 09_async.js/script.js
@@ -1,11 +1,96 @@
+//========================================================
+//                              setTimeout
+//========================================================
+
+
 setTimeout(function(){
     console.log("hui hui");
 
 },2000)
 
-setInterval(function(){
-    console.log(" hui");
-    console.log(" sui");
+// --------------using function refrence-----------
+// fucntion creation
+            // const Hurr = function(){
+            //         console.log("hui hui");
+                
+            //     }
+
+            // const changetext = function(){
+            //     document.querySelector('h1').innerHTML="how r u"
+            // }
+
+    // setTimeout(handler,time in millisec)
+
+    // dont do Hurr() -> it is function print...
+    // set time need handler i.e. function refrence
+    // setTimeout(Hurr(),2000) 
+    
+    // do only hurr -> this is fucntion refrennce
+    //  setTimeout(Hurr,2000)
+    //    const changed= setTime out(changetext,2000)
 
 
-},2000)
+
+// -----------clearTimeout--> stops settimeout execution------------\
+
+// document.querySelector('#stop').addEventListener('click',function(){
+//          clearTimeout(changed)
+//          console.log("stop")
+// })
+
+
+//========================================================
+//             setinterval(handler,time,parameter)
+//========================================================
+
+// setInterval(function(){
+        //         console.log("ashu",Date.now());
+        // },2000)
+        
+        //-----------using refrence-------
+        // const name = function(){
+                //         console.log("ashu",Date.now());
+                // }
+
+                // setInterval(name,2000)
+                
+                
+//-----------------using paramter------
+// const name = function(str){
+        //         console.log(str,Date.now());
+// }
+// setInterval(name,2000,'ashuuuuuuuuuuu')
+
+// clearInterval(run)
+
+//===================================================================
+//         Project = Fucntionality to START STOP button for execution
+//=====================================================================
+
+let name = function(str){
+        console.log(str);
+        setInterval(function(){
+                document.querySelector('body').style.backgroundColor='pink'
+        },1500)
+        setInterval(function(){
+                document.querySelector('body').style.backgroundColor='red'
+        },1500)
+
+}
+let run;
+
+document.querySelector('#start').addEventListener('click',function(){
+        run = setInterval(name,3000,'hello')
+   
+})
+
+document.querySelector('#stop').addEventListener('click',function(){
+        clearInterval(run)
+        console.log('stopped')
+
+})
+
+
+
+
+ 
